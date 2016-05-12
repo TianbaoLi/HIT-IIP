57a0dc71a8313e9498a6b37f71cd979711d03843
TuringMacLee <turingmac@hotmail.com>
2016-05-12 21:37:26 +0800

    Standardize by minmax and decimal

--- 2.3/weather/datapre.py
+++ 2.3/weather/datapre.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import copy
+import math
 
 BASE_DIR = os.path.dirname(__file__)
 file_path = os.path.join(BASE_DIR, 'TrainingData.csv')
@@ -29,8 +30,39 @@ def missingHandler_avg(data):
         if x[selectedAttr] == 'NA':
             x[selectedAttr] = "%f" % avg
     return dataList
+def standardize_minmax(data):
+    dataList = []
+    dataList = copy.deepcopy(data)
+    dataMin = 100.0
+    dataMax = -100.0
+    for x in dataList:
+        if x[selectedAttr] == 'NA' or x[selectedAttr] == "-1000000":
+            continue
+        if float(x[selectedAttr]) < dataMin:
+            dataMin = float(x[selectedAttr])
+        if float(x[selectedAttr]) > dataMax:
+            dataMax = float(x[selectedAttr])
+    for x in dataList:
+        x[selectedAttr] = "%.15f" % ((float(x[selectedAttr]) - dataMin) / (dataMax - dataMin) * (1 - 0) + 0)
+    return dataList
+
+def standardize_decimal(data):
+    dataList = []
+    dataList = copy.deepcopy(data)
+    dataMax = -100.0
+    for x in dataList:
+        if x[selectedAttr] == 'NA' or x[selectedAttr] == "-1000000":
+            continue
+        if float(x[selectedAttr]) > dataMax:
+            dataMax = float(x[selectedAttr])
+    p = pow(10, int(math.log10(dataMax)) + 1)
+
+    for x in dataList:
+        x[selectedAttr] = "%.15f" % (float(x[selectedAttr]) / p)
+    return dataList
 
 numControl = 40
+#Number of lines to be tested
 index = 0
 
 for line in reader:
@@ -40,6 +72,10 @@ for line in reader:
     data.append(line)
 
 data_missingHandeledByGlobal = missingHandler_global(data)
-#print data_missingHandeledByGlobal
 data_missingHandeledByAvg = missingHandler_avg(data)
-#print data_missingHandeledByAvg
+
+data_standardize_minmax = standardize_minmax(data_missingHandeledByAvg)
+data_standardize_decimal= standardize_decimal(data_missingHandeledByAvg)
+
+#for x in data_standardize_decimal:
+#    print x[selectedAttr]

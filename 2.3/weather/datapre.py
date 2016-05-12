5d4c58a80af8301df75efc40da7ce739511e6e80
TuringMacLee <turingmac@hotmail.com>
2016-05-12 20:53:04 +0800

    Missing handled by global and avg

--- 2.3/weather/datapre.py
+++ 2.3/weather/datapre.py
@@ -1,8 +1,45 @@
 import os
 import csv
+import copy
 
 BASE_DIR = os.path.dirname(__file__)
 file_path = os.path.join(BASE_DIR, 'TrainingData.csv')
 reader = csv.DictReader(file(file_path,'rb'))
+data = []
+selectedAttr = 'target_11_1601'
+
+def missingHandler_global(data):
+    dataList = []
+    dataList = copy.deepcopy(data)
+    for x in dataList:
+        if x[selectedAttr] == 'NA':
+            x[selectedAttr] = "-1000000"
+            #Selected transformaed number by www.kaggle.com
+    return dataList
+
+def missingHandler_avg(data):
+    dataList = []
+    dataList = copy.deepcopy(data)
+    avg = 0.0
+    for x in dataList:
+        if x[selectedAttr] != 'NA':
+            avg += float(x[selectedAttr])
+    avg /= len(dataList)
+    for x in dataList:
+        if x[selectedAttr] == 'NA':
+            x[selectedAttr] = "%f" % avg
+    return dataList
+
+numControl = 40
+index = 0
+
 for line in reader:
-    print line
+    index += 1
+    if index > numControl:
+        break
+    data.append(line)
+
+data_missingHandeledByGlobal = missingHandler_global(data)
+#print data_missingHandeledByGlobal
+data_missingHandeledByAvg = missingHandler_avg(data)
+#print data_missingHandeledByAvg
